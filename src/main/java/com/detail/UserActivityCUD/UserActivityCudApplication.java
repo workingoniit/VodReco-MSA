a06722b677330c533ebc6f21eb95cac82d4c17bf
workingoniit <knsy2465@gmail.com>
2024-02-13 03:21:46 +0900

    initial commit

---
+++ src/main/java/com/detail/UserActivityCUD/UserActivityCudApplication.java
@@ -0,0 +1,13 @@
+package com.detail.UserActivityCUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserActivityCudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserActivityCudApplication.class, args);
+	}
+
+}
