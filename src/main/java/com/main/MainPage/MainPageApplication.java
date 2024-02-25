3928d8349d2a6fc28afd16efd6735d050042d390
Seyeon Hwang <knsy2465@gmail.com>
2024-02-26 01:10:37 +0900

    [Chore]: enable MongoRepositories

--- src/main/java/com/main/MainPage/MainPageApplication.java
+++ src/main/java/com/main/MainPage/MainPageApplication.java
@@ -2,8 +2,10 @@ package com.main.MainPage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.main.MainPage.mongoRepository")
 public class MainPageApplication {
 
 	public static void main(String[] args) {
